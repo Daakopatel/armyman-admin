1d595fb420fa9cf7d40b462beb6e812601eaf025
Daakopatel <makasanadarshit092@gmail.com>
2020-12-18 18:09:42 +0530

    changes in app url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: "https://product-slide-api.dakasapps.com/",
-  appUrl: "https://product-slide-home.dakasapps.com/",
+  appUrl: "https://product-slide.dakasapps.com/",
   appName: "Product Preview "
 };
