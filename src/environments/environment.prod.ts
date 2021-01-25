597394e4c459ad747475ea3b9e6753338bc2fe76
Daakopatel <makasanadarshit092@gmail.com>
2021-01-25 19:44:53 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://product-slide-api.dakasapps.com/",
-  appUrl: "https://product-slide.dakasapps.com/",
-  appName: "Product Preview "
+  apiUrl: "https://protector-api.dakasapps.com/",
+  appName: "Protector Store Shield"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "https://product-slide-api.dakasapps.com/",
-  appUrl: "https://product-slide.dakasapps.com/",
-  appName: "Product slide"
+  apiUrl: "https://protector-api.dakasapps.com/",
+  appName: "Protector Store Shield"
 };
