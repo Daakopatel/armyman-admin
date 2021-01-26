28095c6f100a7acf7652d9f790cfb16f172589ce
Daakopatel <makasanadarshit092@gmail.com>
2021-01-26 13:59:33 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: "https://protector-api.dakasapps.com/",
-  appUrl: "https://protector.dakasapps.com",
+  appUrl: "https://protector.dakasapps.com/",
   appName: "Product Preview "
 };
